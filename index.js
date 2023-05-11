cdd86b926c4194512caa0ce7e6e2fcffebfa1c0d
Mohammed-5253 <mohammed22091995@gmail.com>
2023-05-11 12:26:09 +0530

    assignment complete upto 23 min

---
+++ index.js
@@ -0,0 +1,21 @@
+// console.log(document);
+//console.dir(document);
+// console.log(document.title);
+// console.log(document.head);
+// console.log(document.URL);
+// console.log(document.domain);
+// document.title = "MAterial Lister";
+// console.log(document.title);
+// console.log(document.body);
+// console.log(document.forms);
+// console.log(document.links);
+// console.log(document.images);
+
+var headerTitles = document.getElementById('header-title');
+var header = document.getElementById('main-header');
+
+console.log(headerTitles);
+header.style.backgroundColor = 'red';
+headerTitles.textContent = 'hello';
+headerTitles.innerText = 'very good';
+header.style.border = 'solid 3px #000';
